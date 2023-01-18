63d13fc4c2c2a2f3082a42aef5dad646910e077e
venkat-devish <venkatsuryakiranv@gmail.com>
2023-01-19 00:13:57 +0530

    add: currying

--- currying/curry.js
+++ currying/curry.js
@@ -15,3 +15,38 @@ function sum(a) {
 }
 
 sum(2)(6)(1);
+
+function evaluate(param) {
+  return function (a) {
+    return function (b) {
+      if (param === "sum") console.log(a + b);
+      else if (param === "multiply") console.log(a * b);
+      else if (param === "divide") console.log(a / b);
+      else if (param === "subtract") console.log(a - b);
+      else console.log("Invalid Operation");
+    };
+  };
+}
+
+evaluate("sum")(4)(2);
+evaluate("multiply")(4)(2);
+evaluate("divide")(4)(2);
+evaluate("subtract")(4)(2);
+
+const sum = (a) => {
+  return (b) => {
+    if (b) return sum(a + b);
+    return a;
+  };
+};
+
+console.log(sum(1)(2)(3)(4)(5)());
+
+function updateElement(id) {
+  return function (content) {
+    document.querySelector("#" + id).textContent = content;
+  };
+}
+
+const update = updateElement("heading");
+update("Curry");
